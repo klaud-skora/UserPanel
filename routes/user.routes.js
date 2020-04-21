d5735f723557815d7c3776887d9db578676e2945
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-21 21:03:54 +0200

    Add new endpoints and their pages

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,5 +9,13 @@ router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', (req, res) => {
+  res.render('settings');
+});
+
 
 module.exports = router;
